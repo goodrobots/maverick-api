e42f7b042c17dc20e152016c327ed1911925f449
fnoop <fnoopdogg@gmail.com>
2018-02-03 06:33:53 +0000

    Clean up schema and support subscriptions

--- api/schema.py
+++ api/schema.py
@@ -1,14 +1,12 @@
 from __future__ import absolute_import
 import graphene
+from rx.subjects import Subject
 
-from .data import get_character, get_droid, get_hero, get_human
-from .data import get_state_message, set_state_message
-from .data import setup
+class StreamState:
+    stream={} # this dict holds the current telem message data for each message type
 
-class Episode(graphene.Enum):
-    NEWHOPE = 4
-    EMPIRE = 5
-    JEDI = 6
+class Subscriptions:
+    stream={} # this dict holds Subject() for each message type
 
 class TelemMessage(graphene.Interface):
     id = graphene.ID()
@@ -26,29 +24,14 @@ class StateMessage(graphene.ObjectType):
     mode = graphene.String()
     system_status = graphene.Int()
     
-
-class Character(graphene.Interface):
-    id = graphene.ID()
-    name = graphene.String()
-    friends = graphene.List(lambda: Character)
-    appears_in = graphene.List(Episode)
-
-    def resolve_friends(self, info):
-        # The character friends is a list of strings
-        return [get_character(f) for f in self.friends]
-
-class Human(graphene.ObjectType):
-
-    class Meta:
-        interfaces = (Character, )
-    home_planet = graphene.String()
-
-
-class Droid(graphene.ObjectType):
-
-    class Meta:
-        interfaces = (Character, )
-    primary_function = graphene.String()
+    @classmethod
+    def create(cls, seq, secs, nsecs, frame_id, connected, armed, guided, mode, system_status):
+        _id = 'State'
+        state_message = cls(id=_id, seq = seq, secs = secs, nsecs = nsecs, frame_id = frame_id, 
+            connected = connected, armed = armed, guided = guided, mode = mode, system_status = system_status)
+        # update the local storage content
+        StreamState.stream[_id] = state_message
+        return state_message
     
 
 class UpdateStateMessage(graphene.Mutation):
@@ -66,58 +49,51 @@ class UpdateStateMessage(graphene.Mutation):
 
     ok = graphene.Boolean()
     state_message = graphene.Field(lambda: StateMessage)
-
-    def mutate(self, info, id, seq, secs, nsecs, frame_id, connected, armed, guided, mode, system_status):
-        state_message = StateMessage(id = id, seq = seq, secs = secs, nsecs = nsecs, frame_id = frame_id, 
-        connected = connected, armed = armed, guided = guided, mode = mode, system_status = system_status)
+    
+    def mutate(self, info = None, **kwargs):
+        state_message = StateMessage.create(**kwargs)
         ok = True
-        set_state_message(id, state_message)
+        # notify subscribers of an update
+        Subscriptions.stream['State'].on_next(state_message)
+        
         return UpdateStateMessage(state_message=state_message, ok=ok)
 
+
 class Mutation(graphene.ObjectType):
-    
     update_state_message = UpdateStateMessage.Field()
 
 
 class Query(graphene.ObjectType):
+    state_message = graphene.Field(StateMessage)
     
-    state_message = graphene.Field(StateMessage,
-                           id=graphene.String()
-                           )
-    
-    hero = graphene.Field(Character,
-                          episode=Episode()
-                          )
-                          
-    human = graphene.Field(Human,
-                           id=graphene.String()
-                           )
-    droid = graphene.Field(Droid,
-                           id=graphene.String()
-                           )
-
-    def resolve_hero(self, info, episode=None):
-        return get_hero(episode)
-
-    def resolve_human(self, info, id):
-        return get_human(id)
-
-    def resolve_droid(self, info, id):
-        return get_droid(id)
-    
-    def resolve_state_message(self, info, id):
-        return get_state_message(id)
+    def resolve_state_message(self, info):
+        return StreamState.stream['State']
   
-# TODO: work this stuff out...      
-# class Subscription(graphene.ObjectType):
-#     class Meta:
-#         description = 'Example subscription for TornadoQL'
+  
+class Subscription(graphene.ObjectType):
+    state_message = graphene.Field(StateMessage)
 
-#     onPost = graphene.Field(StateMessage)
+    def resolve_state_message(self, info):
+        # return the Subject for this message type
+        return Subscriptions.stream['State']
 
-#     def resolve_onPost(root, info):
-#         return set_state_message
+schema = graphene.Schema(query=Query, mutation=Mutation, subscription=Subscription)
 
-setup()
+state_message = StateMessage(
+    id='State',
+    seq = None, # graphene.Int()
+    secs = None, # graphene.Int()
+    nsecs = None, # graphene.Int()
+    frame_id = None, # graphene.String()
+    connected = None, # graphene.Boolean()
+    armed = None, # graphene.Boolean()
+    guided = None, # graphene.Boolean()
+    mode = None, # graphene.String()
+    system_status = None, # graphene.Int()
+)
 
-schema = graphene.Schema(query=Query, mutation=Mutation) # subscription=Subscription
+# init the state message
+StreamState.stream['State'] = state_message
+
+# add a Subject() for the state message
+Subscriptions.stream['State'] = Subject()
