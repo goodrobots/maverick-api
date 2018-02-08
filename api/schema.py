26f631f6d13447b847ca09a10a30f302a4e230ab
Samuel Dudley <dudley.samuel@gmail.com>
2018-02-08 10:47:00 +0000

    Add support for additional mavros messages

--- api/schema.py
+++ api/schema.py
@@ -14,7 +14,8 @@ class TelemMessage(graphene.Interface):
     secs = graphene.Int()
     nsecs = graphene.Int()
     frame_id = graphene.String()
-    
+
+### start State Message
 class StateMessage(graphene.ObjectType):
     class Meta:
         interfaces = (TelemMessage, )
@@ -55,24 +56,262 @@ class UpdateStateMessage(graphene.Mutation):
         # notify subscribers of an update
         Subscriptions.stream['State'].on_next(state_message)
         return UpdateStateMessage(state_message=state_message, ok=ok)
+### end State Message
 
+### start VFR_HUD Message
+class VFRHUDMessage(graphene.ObjectType):
+    class Meta:
+        interfaces = (TelemMessage, )
+    airspeed = graphene.Float()
+    groundspeed = graphene.Float()
+    heading = graphene.Int()
+    throttle = graphene.Float()
+    altitude = graphene.Float()
+    climb = graphene.Float()
+    
+    @classmethod
+    def create(cls, seq, secs, nsecs, frame_id, airspeed, groundspeed, heading, throttle, altitude, climb):
+        _id = 'VFR_HUD'
+        vfr_hud_message = cls(id=_id, seq = seq, secs = secs, nsecs = nsecs, frame_id = frame_id, 
+            airspeed = airspeed, groundspeed = groundspeed, heading = heading, throttle = throttle, altitude = altitude, climb = climb)
+        # update the local storage content
+        StreamState.stream[_id] = vfr_hud_message
+        return vfr_hud_message
+    
+class UpdateVFRHUDMessage(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        seq = graphene.Int()
+        secs = graphene.Int()
+        nsecs = graphene.Int()
+        frame_id = graphene.String()
+        airspeed = graphene.Float()
+        groundspeed = graphene.Float()
+        heading = graphene.Int()
+        throttle = graphene.Float()
+        altitude = graphene.Float()
+        climb = graphene.Float()
+
+    ok = graphene.Boolean()
+    vfr_hud_message = graphene.Field(lambda: VFRHUDMessage)
+    
+    def mutate(self, info = None, **kwargs):
+        vfr_hud_message = VFRHUDMessage.create(**kwargs)
+        ok = True
+        # notify subscribers of an update
+        Subscriptions.stream['VFR_HUD'].on_next(vfr_hud_message)
+        return UpdateVFRHUDMessage(vfr_hud_message=vfr_hud_message, ok=ok)
+### end VFR_HUD Message
+
+### start PoseStamped Message
+class PoseStampedMessage(graphene.ObjectType):
+    class Meta:
+        interfaces = (TelemMessage, )
+    pose_position_x = graphene.Float()
+    pose_position_y = graphene.Float()
+    pose_position_z = graphene.Float()
+    pose_orientation_x = graphene.Float()
+    pose_orientation_y = graphene.Float()
+    pose_orientation_z = graphene.Float()
+    pose_orientation_w = graphene.Float()
+    
+    @classmethod
+    def create(cls, seq, secs, nsecs, frame_id,
+               pose_position_x, pose_position_y, pose_position_z, pose_orientation_x, pose_orientation_y, pose_orientation_z, pose_orientation_w):
+        _id = 'PoseStamped'
+        pose_stamped_message = cls(id=_id, seq = seq, secs = secs, nsecs = nsecs, frame_id = frame_id, 
+            pose_position_x = pose_position_x, pose_position_y = pose_position_y, pose_position_z = pose_position_z,
+            pose_orientation_x = pose_orientation_x, pose_orientation_y = pose_orientation_y, pose_orientation_z = pose_orientation_z)
+        # update the local storage content
+        StreamState.stream[_id] = pose_stamped_message
+        return pose_stamped_message
+    
+class UpdatePoseStampedMessage(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        seq = graphene.Int()
+        secs = graphene.Int()
+        nsecs = graphene.Int()
+        frame_id = graphene.String()
+        pose_position_x = graphene.Float()
+        pose_position_y = graphene.Float()
+        pose_position_z = graphene.Float()
+        pose_orientation_x = graphene.Float()
+        pose_orientation_y = graphene.Float()
+        pose_orientation_z = graphene.Float()
+        pose_orientation_w = graphene.Float()
+
+    ok = graphene.Boolean()
+    pose_stamped_message = graphene.Field(lambda: PoseStampedMessage)
+    
+    def mutate(self, info = None, **kwargs):
+        pose_stamped_message = PoseStampedMessage.create(**kwargs)
+        ok = True
+        # notify subscribers of an update
+        Subscriptions.stream['PoseStamped'].on_next(pose_stamped_message)
+        return UpdatePoseStampedMessage(pose_stamped_message=pose_stamped_message, ok=ok)
+### end PoseStamped Message
+
+### start Imu Message
+class ImuMessage(graphene.ObjectType):
+    class Meta:
+        interfaces = (TelemMessage, )
+    orientation_x = graphene.Float()
+    orientation_y = graphene.Float()
+    orientation_z = graphene.Float()
+    orientation_w = graphene.Float()
+    # TODO: orientation covariance array
+    angular_velocity_x = graphene.Float()
+    angular_velocity_y = graphene.Float()
+    angular_velocity_z = graphene.Float()
+    # TODO: angular velocity covariance array
+    linear_acceleration_x = graphene.Float()
+    linear_acceleration_y = graphene.Float()
+    linear_acceleration_z = graphene.Float()
+    # TODO: linear acceleration covariance array
+    # TODO: calculate and provide roll, pitch, yaw euler angles
+    
+    @classmethod
+    def create(cls, seq, secs, nsecs, frame_id, orientation_x, orientation_y, orientation_z, orientation_w,
+               angular_velocity_x,  angular_velocity_y, angular_velocity_z,
+               linear_acceleration_x, linear_acceleration_y, linear_acceleration_z):
+        _id = 'Imu'
+        imu_message = cls(id=_id, seq = seq, secs = secs, nsecs = nsecs, frame_id = frame_id, 
+            orientation_x = orientation_x, orientation_y = orientation_y, orientation_z = orientation_z, orientation_w = orientation_w,
+            angular_velocity_x = angular_velocity_x, angular_velocity_y = angular_velocity_y, angular_velocity_z = angular_velocity_z,
+            linear_acceleration_x = linear_acceleration_x, linear_acceleration_y = linear_acceleration_y, linear_acceleration_z = linear_acceleration_z)
+        # update the local storage content
+        StreamState.stream[_id] = imu_message
+        return imu_message
+    
+class UpdateImuMessage(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        seq = graphene.Int()
+        secs = graphene.Int()
+        nsecs = graphene.Int()
+        frame_id = graphene.String()
+        orientation_x = graphene.Float()
+        orientation_y = graphene.Float()
+        orientation_z = graphene.Float()
+        orientation_w = graphene.Float()
+        angular_velocity_x = graphene.Float()
+        angular_velocity_y = graphene.Float()
+        angular_velocity_z = graphene.Float()
+        linear_acceleration_x = graphene.Float()
+        linear_acceleration_y = graphene.Float()
+        linear_acceleration_z = graphene.Float()
+
+    ok = graphene.Boolean()
+    imu_message = graphene.Field(lambda: ImuMessage)
+    
+    def mutate(self, info = None, **kwargs):
+        imu_message = ImuMessage.create(**kwargs)
+        ok = True
+        # notify subscribers of an update
+        Subscriptions.stream['Imu'].on_next(imu_message)
+        return UpdateImuMessage(imu_message=imu_message, ok=ok)
+### end Imu Message
+
+### start NavSatFix Message
+class NavSatFixMessage(graphene.ObjectType):
+    class Meta:
+        interfaces = (TelemMessage, )
+    status_status = graphene.Int()
+    status_service = graphene.Int()
+    latitude = graphene.Float()
+    longitude = graphene.Float()
+    altiude = graphene.Float()
+    # TODO: position_covariance array
+    position_covariance_type = graphene.Int()
+    
+    @classmethod
+    def create(cls, seq, secs, nsecs, frame_id, status_status, status_service, latitude, longitude, altiude, position_covariance_type):
+        _id = 'NavSatFix'
+        pose_stamped_message = cls(id=_id, seq = seq, secs = secs, nsecs = nsecs, frame_id = frame_id, status_status = status_status, status_service = status_service,
+        latitude = latitude, longitude = longitude, altiude = altiude, position_covariance_type = position_covariance_type)
+        # update the local storage content
+        StreamState.stream[_id] = pose_stamped_message
+        return pose_stamped_message
+    
+class UpdateNavSatFixMessage(graphene.Mutation):
+    class Arguments:
+        id = graphene.ID()
+        seq = graphene.Int()
+        secs = graphene.Int()
+        nsecs = graphene.Int()
+        frame_id = graphene.String()
+        status_status = graphene.Int()
+        status_service = graphene.Int()
+        latitude = graphene.Float()
+        longitude = graphene.Float()
+        altiude = graphene.Float()
+        # TODO: position_covariance array
+        position_covariance_type = graphene.Int()
+
+    ok = graphene.Boolean()
+    nav_sat_fixed_message = graphene.Field(lambda: NavSatFixMessage)
+    
+    def mutate(self, info = None, **kwargs):
+        nav_sat_fixed_message = NavSatFixMessage.create(**kwargs)
+        ok = True
+        # notify subscribers of an update
+        Subscriptions.stream['NavSatFix'].on_next(nav_sat_fixed_message)
+        return UpdateNavSatFixMessage(nav_sat_fixed_message=nav_sat_fixed_message, ok=ok)
+### end NavSatFix Message
 
 class Mutation(graphene.ObjectType):
     update_state_message = UpdateStateMessage.Field()
-
+    update_vfr_hud_message = UpdateVFRHUDMessage.Field()
+    update_pose_stamped_message = UpdatePoseStampedMessage.Field()
+    update_nav_sat_fixed_message = UpdateNavSatFixMessage.Field()
+    update_imu_message = UpdateImuMessage.Field()
 
 class Query(graphene.ObjectType):
     state_message = graphene.Field(StateMessage)
+    vfr_hud_message = graphene.Field(VFRHUDMessage)
+    pose_stamped_message = graphene.Field(PoseStampedMessage)
+    nav_sat_fixed_message = graphene.Field(NavSatFixMessage)
+    imu_message = graphene.Field(ImuMessage)
     
     def resolve_state_message(self, info):
         return StreamState.stream['State']
-  
+        
+    def resolve_vfr_hud_message(self, info):
+        return StreamState.stream['VFR_HUD']
+        
+    def resolve_pose_stamped_message(self, info):
+        return StreamState.stream['PoseStamped']
+    
+    def resolve_nav_sat_fixed_message(self, info):
+        return StreamState.stream['NavSatFix']
+    
+    def resolve_imu_message(self, info):
+        return StreamState.stream['Imu']
+        
   
 class Subscription(graphene.ObjectType):
     state_message = graphene.Field(StateMessage)
+    vfr_hud_message = graphene.Field(VFRHUDMessage)
+    pose_stamped_message = graphene.Field(PoseStampedMessage)
+    nav_sat_fixed_message = graphene.Field(NavSatFixMessage)
+    imu_message = graphene.Field(ImuMessage)
 
     def resolve_state_message(self, info):
         return Subscriptions.stream['State']
+    
+    def resolve_vfr_hud_message(self, info):
+        return Subscriptions.stream['VFR_HUD']
+        
+    def resolve_pose_stamped_message(self, info):
+        return Subscriptions.stream['PoseStamped']
+    
+    def resolve_nav_sat_fixed_message(self, info):
+        return Subscriptions.stream['NavSatFix']
+        
+    def resolve_imu_message(self, info):
+        return Subscriptions.stream['Imu']
+        
 
 schema = graphene.Schema(query=Query, mutation=Mutation, subscription=Subscription)
 
@@ -89,8 +328,82 @@ state_message = StateMessage(
     system_status = None, # graphene.Int()
 )
 
+vfr_hud_message = VFRHUDMessage(
+    id = 'VFR_HUD',
+    seq = None, # graphene.Int()
+    secs = None, # graphene.Int()
+    nsecs = None, # graphene.Int()
+    frame_id = None, # graphene.String()
+    airspeed = None, # graphene.Float()
+    groundspeed = None, # graphene.Float()
+    heading = None, # graphene.Int()
+    throttle = None, # graphene.Float()
+    altitude = None, # graphene.Float()
+    climb = None, # graphene.Float()
+)
+
+pose_stamped_message = PoseStampedMessage(
+    id = 'PoseStamped',
+    seq = None, # graphene.Int()
+    secs = None, # graphene.Int()
+    nsecs = None, # graphene.Int()
+    frame_id = None, # graphene.String()
+    pose_position_x = None, # graphene.Float()
+    pose_position_y = None, #  graphene.Float()
+    pose_position_z = None, #  graphene.Float()
+    pose_orientation_x = None, #  graphene.Float()
+    pose_orientation_y = None, #  graphene.Float()
+    pose_orientation_z = None, #  graphene.Float()
+    pose_orientation_w = None, #  graphene.Float()
+)
+
+nav_sat_fixed_message = NavSatFixMessage(
+    id = 'NavSatFix',
+    seq = None, # graphene.Int()
+    secs = None, # graphene.Int()
+    nsecs = None, # graphene.Int()
+    frame_id = None, # graphene.String()
+    status_status = None, # graphene.Int()
+    status_service = None, # graphene.Int()
+    latitude = None, # graphene.Float()
+    longitude = None, # graphene.Float()
+    altiude = None, # graphene.Float()
+    position_covariance_type = None, # graphene.Int()
+)
+
+imu_message = ImuMessage(
+    id = 'Imu',
+    seq = None, # graphene.Int()
+    secs = None, # graphene.Int()
+    nsecs = None, # graphene.Int()
+    frame_id = None, # graphene.String()
+    orientation_x = None, # graphene.Float()
+    orientation_y = None, # graphene.Float()
+    orientation_z = None, # graphene.Float()
+    orientation_w = None, #graphene.Float()
+    angular_velocity_x = None, # graphene.Float()
+    angular_velocity_y = None, # graphene.Float()
+    angular_velocity_z = None, # graphene.Float()
+    linear_acceleration_x = None, # graphene.Float()
+    linear_acceleration_y = None, # graphene.Float()
+    linear_acceleration_z = None, # graphene.Float()
+)
+
+ 
 # init the state message
 StreamState.stream['State'] = state_message
+# init the VFR_HUD message
+StreamState.stream['VFR_HUD'] = vfr_hud_message
+# init the pose stamped message
+StreamState.stream['PoseStamped'] = pose_stamped_message
+# init the nav sat fixed message
+StreamState.stream['NavSatFix'] = nav_sat_fixed_message
+# init the imu message
+StreamState.stream['Imu'] = imu_message
 
-# add a Subject() for the state message
+# add Subjects for all message types
 Subscriptions.stream['State'] = Subject()
+Subscriptions.stream['VFR_HUD'] = Subject()
+Subscriptions.stream['PoseStamped'] = Subject()
+Subscriptions.stream['NavSatFix'] = Subject()
+Subscriptions.stream['Imu'] = Subject()
