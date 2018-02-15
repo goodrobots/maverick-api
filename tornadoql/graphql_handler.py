e76ca0892fce1ee799273e4d47bcab4fe6340df8
fnoop <fnoopdogg@gmail.com>
2018-02-15 23:28:46 +0000

    Add requesthandler CORS support, #9

--- tornadoql/graphql_handler.py
+++ tornadoql/graphql_handler.py
@@ -63,6 +63,12 @@ class ExecutionError(Exception):
 
 
 class GQLHandler(web.RequestHandler):
+    def set_default_headers(self):
+        self.set_header("Access-Control-Allow-Origin", "*")
+        self.set_header("Access-Control-Allow-Credentials", "true")
+        self.set_header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
+        self.set_header('Access-Control-Allow-Methods', 'HEAD, PUT, POST, GET, OPTIONS')
+    
     def options(self):
         self.set_status(204)
         self.finish()
